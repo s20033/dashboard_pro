cf40ea719ea957439969e4dc9d4bfab76bfdd564
s20033 <s20033@pjwstk.edu.pl>
2023-08-03 23:13:42 +0100

    first commit

---
+++ client/src/components/BoxHeader.tsx
@@ -0,0 +1,34 @@
+// import React from 'react'
+import { Box, Typography, useTheme } from '@mui/material'
+import FlexBetween from './FlexBetween'
+
+type Props = {
+    title: string;
+    subtitle: string;
+    sidetext?:string;
+    icon?:React.ReactNode;
+}
+
+const BoxHeader = ({icon, title,subtitle,sidetext}: Props) => {
+    const {palette}= useTheme();
+  return (
+   <FlexBetween color ={palette.grey[400]}
+   margin="1.5rem 1rem 0 1rem">
+    <FlexBetween>
+        {icon}
+        <Box width="100%">
+            <Typography variant='h4' mb="-0.1rem">
+                {title}
+            </Typography>
+            <Typography variant='h6'>{subtitle}</Typography>
+        </Box>
+    </FlexBetween>
+    <Typography variant='h5' fontWeight="700" color={palette.secondary[500]}>
+        {sidetext}
+    </Typography>
+
+   </FlexBetween>
+  )
+}
+
+export default BoxHeader
